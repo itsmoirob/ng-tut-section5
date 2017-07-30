e4c5bbce3b4f76c04bc2e9c1a74e119526bb8362
Robbie <rob.w.sherman@gmail.com>
2017-07-30 15:18:47 +0100

    finish section 8

--- src/app/course-form/course-form.component.ts
+++ src/app/course-form/course-form.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms/';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.css']
 })
-export class CourseFormComponent implements OnInit {
+export class CourseFormComponent {
 
-  constructor() { }
+  // form = new FormGroup({
+  //   name: new FormControl(),
+  //   contact: new FormGroup({
+  //     email: new FormControl(),
+  //     phone: new FormControl()
+  //   }),
+  //   topics: new FormArray([])
+  // });
 
-  ngOnInit() {
+  form;
+
+  // instad of building forms like above you can use form builder
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
+  }
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  removeTopic(topic: FormControl) {
+    const index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
   }
 
 }
