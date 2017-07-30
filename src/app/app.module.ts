f6c646454b96b9b4ba3eed9378a34fb15548bea9
Robbie <rob.w.sherman@gmail.com>
2017-07-30 13:37:14 +0100

    Add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FavouriteComponent } from './favourite/favourite.component';
-import { SignupFormComponent  } from './signup-form/signup-form.component'; 
+import { SignupFormComponent  } from './signup-form/signup-form.component';
+import { CourseFormComponent } from './course-form/course-form.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FavouriteComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule,
