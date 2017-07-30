eeab581e80cfbd9673eab46a50c9f3a2a2312500
Robbie <rob.w.sherman@gmail.com>
2017-07-30 13:36:38 +0100

    Update section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FavouriteComponent } from './favourite/favourite.component';
+import { SignupFormComponent  } from './signup-form/signup-form.component'; 
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavouriteComponent
+    FavouriteComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
